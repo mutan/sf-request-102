b1c897f5f9cc1f97e15b752a754a337d9a2cc21f
Akim Gubanov <akim.gubanov@gmail.com>
2021-02-26 19:35:15 +0300

    Fixed test

--- tests/Unit/Service/TokenGeneratorServiceTest.php
+++ tests/Unit/Service/TokenGeneratorServiceTest.php
@@ -20,43 +20,43 @@ class TokenGeneratorServiceTest extends TestCase
     public function testGetToken(): void
     {
         $token = $this->tokenGenerator->getToken(16);
-        $this->assertInternalType('string', $token);
-        $this->assertTrue(16 == \strlen($token));
-        $this->assertRegExp('/^[a-zA-Z0-9]*$/', $token);
+        self::assertIsString($token);
+        self::assertSame(16, \strlen($token));
+        self::assertRegExp('/^[a-zA-Z0-9]*$/', $token);
     }
 
     public function testGetHexadecimalToken(): void
     {
         $token = $this->tokenGenerator->getHexadecimalToken(16);
-        $this->assertInternalType('string', $token);
-        $this->assertTrue(16 == \strlen($token));
-        $this->assertRegExp('/^[a-f0-9]*$/', $token);
+        self::assertIsString($token);
+        self::assertSame(16, \strlen($token));
+        self::assertRegExp('/^[a-f0-9]*$/', $token);
     }
 
     public function testGetPassword(): void
     {
         $token = $this->tokenGenerator->getPassword(10);
-        $this->assertInternalType('string', $token);
-        $this->assertTrue(10 == \strlen($token));
-        $this->assertRegExp('/^[a-zA-Z0-9]*$/', $token);
+        self::assertIsString($token);
+        self::assertEquals(10, \strlen($token));
+        self::assertRegExp('/^[a-zA-Z0-9]*$/', $token);
     }
 
     public function testGetCustomPassword(): void
     {
         $token = $this->tokenGenerator->getCustomPassword(12, 3, 3, 3, 3);
-        $this->assertInternalType('string', $token);
-        $this->assertTrue(12 == \strlen($token));
-        $this->assertTrue(3 == preg_match_all('/[a-z]/', $token));
-        $this->assertTrue(3 == preg_match_all('/[A-Z]/', $token));
-        $this->assertTrue(3 == preg_match_all('/[0-9]/', $token));
-        $this->assertTrue(3 == preg_match_all("/[\+\-_=!#$%&?@~]/", $token));
+        self::assertIsString($token);
+        self::assertEquals(12, \strlen($token));
+        self::assertEquals(3, preg_match_all('/[a-z]/', $token));
+        self::assertEquals(3, preg_match_all('/[A-Z]/', $token));
+        self::assertEquals(3, preg_match_all('/[0-9]/', $token));
+        self::assertEquals(3, preg_match_all("/[\+\-_=!#$%&?@~]/", $token));
     }
 
     public function testGetCustomToken(): void
     {
         $token = $this->tokenGenerator->getCustomToken(64, 15);
-        $this->assertInternalType('string', $token);
-        $this->assertTrue(64 == \strlen($token));
-        $this->assertRegExp("/^[a-zA-Z0-9\+\-_=!#$%&?@~]*$/", $token);
+        self::assertIsString($token);
+        self::assertSame(64, \strlen($token));
+        self::assertRegExp("/^[a-zA-Z0-9\+\-_=!#$%&?@~]*$/", $token);
     }
 }
