022d4a2730c4b57e020c0c1a024946b90f9efc8e
Akim Gubanov <akim.gubanov@gmail.com>
2021-03-15 00:27:53 +0300

    Added cache to getTeamsMap

--- src/Service/Entity/NFLTeamManager.php
+++ src/Service/Entity/NFLTeamManager.php
@@ -8,6 +8,8 @@ use App\Factory\NFLTeamFactory;
 use App\Repository\NFLTeamRepository;
 use App\Service\Api\SportsdataApiService;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -16,6 +18,8 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class NFLTeamManager
 {
+    private const TEAM_MAP_CACHE_KEY = 'team_map';
+
     private NFLTeamRepository $repository;
 
     private NFLTeamFactory $factory;
@@ -24,13 +28,17 @@ class NFLTeamManager
 
     private EntityManagerInterface $em;
 
+    private CacheItemPoolInterface $cachePool;
+
     public function __construct(
         EntityManagerInterface $em,
+        CacheItemPoolInterface $cachePool,
         NFLTeamRepository $repository,
         NFLTeamFactory $factory,
         SportsdataApiService $api
     ) {
         $this->em = $em;
+        $this->cachePool = $cachePool;
         $this->repository = $repository;
         $this->factory = $factory;
         $this->api = $api;
@@ -39,6 +47,7 @@ class NFLTeamManager
     /**
      * @throws ClientExceptionInterface
      * @throws DecodingExceptionInterface
+     * @throws InvalidArgumentException
      * @throws RedirectionExceptionInterface
      * @throws ServerExceptionInterface
      * @throws TransportExceptionInterface
@@ -46,11 +55,11 @@ class NFLTeamManager
     public function updateTeamListByApi(): void
     {
         $data = $this->api->fetchNFLTeams();
-        $teamMap = $this->getTeamMap();
+        $teamsMap = $this->getTeamsMap();
 
         foreach ($data as $item) {
-            if (isset($teamMap[$item['TeamID']])) {
-                $NFLTeam = $this->factory->updateEntityFromArray($teamMap[$item['TeamID']], $item);
+            if (isset($teamsMap[$item['TeamID']])) {
+                $NFLTeam = $this->factory->updateEntityFromArray($teamsMap[$item['TeamID']], $item);
             } else {
                 $NFLTeam = $this->factory->createEntityFromArray($item);
             }
@@ -60,15 +69,25 @@ class NFLTeamManager
         $this->em->flush();
     }
 
-    private function getTeamMap(): array
+    /**
+     * @throws InvalidArgumentException
+     */
+    private function getTeamsMap(): array
     {
-        $teamMap = [];
-        $teams = $this->repository->findAll();
+        $item = $this->cachePool->getItem(self::TEAM_MAP_CACHE_KEY);
 
-        foreach ($teams as $team) {
-            $teamMap[$team->getTeamId()] = $team;
+        if (!$item->isHit()) {
+            $teamsMap = [];
+            $teams = $this->repository->findAll();
+
+            foreach ($teams as $team) {
+                $teamsMap[$team->getTeamId()] = $team;
+            }
+
+            $item->set($teamsMap);
+            $this->cachePool->save($item);
         }
 
-        return $teamMap;
+        return $item->get();
     }
 }
