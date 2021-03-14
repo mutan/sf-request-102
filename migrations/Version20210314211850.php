1c3c0f022a2e6697e8036001f47f5fc7e801599a
Akim Gubanov <akim.gubanov@gmail.com>
2021-03-15 00:20:15 +0300

    Changed migrations

--- migrations/Version20210314211850.php
+++ migrations/Version20210314211850.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210312220035 extends AbstractMigration
+final class Version20210314211850 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,17 +20,20 @@ final class Version20210312220035 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE nflteam_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE nfl_team_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE nfl_team (id INT NOT NULL, key VARCHAR(255) NOT NULL, team_id INT NOT NULL, city VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, conference VARCHAR(255) DEFAULT NULL, division VARCHAR(255) DEFAULT NULL, full_name VARCHAR(255) DEFAULT NULL, head_coach VARCHAR(255) DEFAULT NULL, offensive_scheme VARCHAR(10) DEFAULT NULL, defensive_scheme VARCHAR(10) DEFAULT NULL, logo_url VARCHAR(255) DEFAULT NULL, word_mark_url VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_68F4FA07296CD8AE ON nfl_team (team_id)');
+        $this->addSql('CREATE TABLE warehouse (id INT NOT NULL, code VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, active BOOLEAN DEFAULT \'true\' NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_ECB38BFC77153098 ON warehouse (code)');
+        $this->addSql('COMMENT ON TABLE warehouse IS \'Склады\'');
+        $this->addSql('COMMENT ON COLUMN warehouse.code IS \'Код склада\'');
+        $this->addSql('COMMENT ON COLUMN warehouse.name IS \'Название склада\'');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE nfl_team_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE nflteam_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE nfl_team');
+        $this->addSql('DROP TABLE warehouse');
     }
 }
